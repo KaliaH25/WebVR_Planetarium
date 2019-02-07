f5a90e67306e4c4f320a001072818765f8593d9c
KaliaH25 <kaliahameiri@gmail.com>
2019-02-07 08:45:41 -0500

    Working with the space ship interaction component

--- public/js/shine_light_beam.js
+++ public/js/shine_light_beam.js
@@ -10,22 +10,25 @@ AFRAME.registerComponent('shine_light_beam',{
     init: function(){
         const Context_AF = this;
         // define this for important reasons that i dont understand
-        //Context_AF.funSound = document.querySelector('#funSound');
+        Context_AF.sfx = document.querySelector('#beam-sound');
 
         Context_AF.el.addEventListener('click', function(){
             console.log('click');
-            Context_AF.lightBeam();
-            //Context_AF.funSound.components['sound'].playSound();
+            Context_AF.createLightBeam();
+            Context_AF.sfx.components['sound'].playSound();
+        });
+        Context_AF.el.addEventListener('mouseleave',function(){
+            console.log('mouseUp');
+            Context_AF.playRemoveAnimation();
+            Context_AF.removeLightBeam();
         });
     },
-    lightBeam : function (){
-        //lets create a cow and the world a better place :) 
+    createLightBeam : function (){
+        //creates cone to act as light beam
         const Context_AF = this;
 
-        
-        
         let beam = document.createElement('a-cone'); //create a new element in memory
-        //<a-cone color="tomato" radius-bottom="2" radius-top="0.5"></a-cone>
+        
         beam.setAttribute('id',"beam");
         beam.setAttribute('color',"green");
         beam.setAttribute('radius-bottom',0.2);
@@ -33,20 +36,42 @@ AFRAME.registerComponent('shine_light_beam',{
         beam.setAttribute('position',{x:Context_AF.el.getAttribute('position').x, y:Context_AF.el.getAttribute('position').y, z:Context_AF.el.getAttribute('position').z});
         beam.setAttribute('material',{ transparency: "true",opacity: 0.5});
         beam.setAttribute('height',0);
+        //beam.setAttribute('remove-light',{});//component to delete self
 
         //append the light to the html
         const scene = document.querySelector('a-scene'); //finds scene in html
         scene.appendChild(beam); //appends it 
 
         let anime = document.createElement('a-animation');
-        //attribute="rotation" from="0 0 0" to="0 360 0" dur="5000"
+        //animation to play on creation
         anime.setAttribute("attribute","height");
         anime.setAttribute("from",0);
         anime.setAttribute("to",2);
         anime.setAttribute("dur",1000);
         
-        //const cone= document.querySelector('a-cone'); //finds scene in html
+        
         beam.appendChild(anime); //appends it
         
+    },
+
+    removeLightBeam: function(){
+        const beam = document.querySelector('#beam');//save beam 
+        beam.parentNode.removeChild(beam);//remove beam from scene
+        console.log('Item removed');
+
+    },
+    playRemoveAnimation:function(){
+        const Context_AF = this;
+
+        const beam = document.querySelector('#beam');//save beam 
+        
+        let anime = document.createElement('a-animation');
+        //animation to play on removal
+        anime.setAttribute("attribute","height");
+        anime.setAttribute("from",2);
+        anime.setAttribute("to",0);
+        anime.setAttribute("dur",1000);
+
+        beam.appendChild(anime);
     }
 });
