3002722a23b7a025efb85690a507e07def031410
KaliaH25 <kaliahameiri@gmail.com>
2019-02-08 11:24:52 -0500

    added spin sound

--- public/js/spin_sfx.js
+++ public/js/spin_sfx.js
@@ -9,5 +9,31 @@ AFRAME.registerComponent('spin_sfx',{
         Context_AF.sfx = document.querySelector('#spin-sound');
 
         document.querySelector('#plntSpin').addEventListener('animationstart',function(){
+            let sndPos = Context_AF.getPos();
+            console.log("playsound?");
+            Context_AF.sfx.setAttribute("position",sndPos);
+            Context_AF.sfx.components['sound'].playSound();
+            Context_AF.sfx.removeAttribute("position");
+
+        });
+        document.querySelector('#astrSpin').addEventListener('animationstart',function(){
+            let sndPos = Context_AF.getPos();
+            console.log("playsound?");
+            Context_AF.sfx.setAttribute("position",sndPos);
+            Context_AF.sfx.components['sound'].playSound();
+            Context_AF.sfx.removeAttribute("position");
+
+        });
     },
+    getPos: function(){
+        const Context_AF = this;
+        let x,y,z; 
+
+        x = Context_AF.el.getAttribute('position').x;
+        y = Context_AF.el.getAttribute('position').y;
+        z = Context_AF.el.getAttribute('position').z;
+        let xyz = x.toString() + " "+ y.toString() + " " + z.toString();
+
+        return xyz;
+    }
 });
